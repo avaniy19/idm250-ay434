c60ad5a781e1c45d4dfa0900b45c62f5ee1e9437
avani <ay434@drexel.edu>
2023-02-27 18:10:34 -0500

    assignment 3

--- wp-content/themes/idm250-theme/footer.php
+++ wp-content/themes/idm250-theme/footer.php
@@ -1,2 +1,12 @@
+<footer>
+  <p>&copy; <?php echo date('Y'); ?>
+    <?php bloginfo('name'); ?>
+  </p>
+  <br>
+  <?php wp_nav_menu(['theme_location' => 'footer-menu']); ?>
+</footer>
+<?php // https://developer.wordpress.org/reference/functions/wp_footer/?>
+<?php wp_footer(); ?>
 </body>
+
 </html>
--- wp-content/themes/idm250-theme/header.php
+++ wp-content/themes/idm250-theme/header.php
@@ -1,9 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>idm250</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Bellota:ital@1&family=Codystar&family=Gideon+Roman&family=Gruppo&display=swap" rel="stylesheet">  
+  <title>idm250</title>
+  <?php wp_head(); ?>
 </head>
-<body>
+
+<body <?php body_class(); ?>>
+  <?php
+  // @link https://developer.wordpress.org/reference/functions/wp_body_open/
+  // Fires the wp_body_open action.
+  wp_body_open();
+  ?>
+
+<nav>
+  <div class="logo">
+    <a href="#">
+      starry⁂eyed
+      <!-- <img src="logo.png" alt="Logo"> -->
+    </a>
+  </div>
+<div class="menu">
+  <ul>
+    <li><a href="#">＊ daily horoscopes</a></li>
+    <li><a href="#">＊ zodiac signs</a></li>
+    <li><a href="#">＊ explore more</a></li>
+
+    
+    <!-- <li><a href="#">contact</a></li> -->
+  </ul>
+  </div>
+</nav>
